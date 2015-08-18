2525d4bed5683b2f480ed6fbc61190a0464e3c21
Kurt De Vocht <kurt.de.vocht@pandora.be>
2015-08-18 14:18:27 +0200

    Initial version - no docu yet...

---
+++ src/ScratchCanTalk/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace ScratchCanTalk
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = 15003;
+
+            var options = new StartOptions()
+            {
+                ServerFactory = "Nowin",
+                Port = port
+            };
+
+            using (WebApp.Start<Startup>(options))
+            {
+                Console.WriteLine("All OK!");
+                Console.WriteLine("Running a http server on port " + port);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ src/ScratchCanTalk/ScratchController.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Web.Http;
+
+namespace ScratchCanTalk
+{
+    public class ScratchController : ApiController
+    {
+        [Route("poll")]
+        [HttpGet]
+        public HttpResponseMessage Poll()
+        {
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            var content = new StringBuilder();
+
+            // content.Append("sensorX 123\n");
+
+            resp.Content = new StringContent(content.ToString(), Encoding.UTF8, "text/plain");
+            return resp;
+        }
+
+        [Route("speak/{thingToSay}/{gender}")]
+        [HttpGet]
+        public HttpResponseMessage Speak(string thingToSay, string gender)
+        {
+            var synth = this.BuildSynth(gender);
+            synth.SpeakAsync(thingToSay);
+            return this.OkResponse;
+        }
+
+        private SpeechSynthesizer BuildSynth(string gender)
+        {
+            var synth = new SpeechSynthesizer();
+            if (!string.IsNullOrEmpty(gender) && gender.ToUpper() == "BOY")
+            {
+                synth.SelectVoiceByHints(VoiceGender.Male);
+            }
+            else
+            {
+                synth.SelectVoiceByHints(VoiceGender.Female);
+            }
+
+            return synth;
+        }
+
+        private HttpResponseMessage OkResponse
+        {
+            get
+            {
+                var resp = new HttpResponseMessage(HttpStatusCode.OK);
+                resp.Content = new StringContent(string.Empty, Encoding.UTF8, "text/plain");
+                return resp;
+            }
+        }
+    }
+}
