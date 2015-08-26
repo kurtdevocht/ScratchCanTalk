0d618485964ddfe42caf1d14855cd3d9fe2cd601
Kurt De Vocht <kurt.de.vocht@pandora.be>
2015-08-26 21:33:11 +0200

    Added nice welcome message

--- src/ScratchCanTalk/Program.cs
+++ src/ScratchCanTalk/Program.cs
@@ -7,6 +7,16 @@ namespace ScratchCanTalk
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("");
+
+
+            Console.WriteLine(@"   _____                _       _      _____         _______    _ _    ");
+            Console.WriteLine(@"  / ____|              | |     | |    / ____|       |__   __|  | | |   ");
+            Console.WriteLine(@" | (___   ___ _ __ __ _| |_ ___| |__ | |     __ _ _ __ | | __ _| | | __");
+            Console.WriteLine(@"  \___ \ / __| '__/ _` | __/ __| '_ \| |    / _` | '_ \| |/ _` | | |/ /");
+            Console.WriteLine(@"  ____) | (__| | | (_| | || (__| | | | |___| (_| | | | | | (_| | |   <");
+            Console.WriteLine(@" |_____/ \___|_|  \__,_|\__\___|_| |_|\_____\__,_|_| |_|_|\__,_|_|_|\_\");
+
             var port = 15003;
 
             var options = new StartOptions()
