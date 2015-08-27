78f00e29650b8f0ed0fac83c430a1302dee41d1e
Kurt De Vocht <kurt.de.vocht@pandora.be>
2015-08-27 09:23:16 +0200

    Added readme, added bin, cleanup of gitignore

---
+++ README.md
@@ -0,0 +1,25 @@
+# ScratchCanTalk
+Are you a bit disappointed that the Scratch 'Say'-block doesn't actually say anything? This extension introduces an additional 'Say' block that uses text-to-speech! 
+Windows only...
+
+##Step 1 - Make sure .Net framework 4.5.1 is installed
+Installed by default on Windows 8.1 and higher. If needed, it can be installed from http://www.microsoft.com/en-US/download/details.aspx?id=40779
+
+##Step 2 - Run the Scratch helper application
+Build the Scratch helper application (Visual studio 2015) or run the binary 'sct.exe' that can be found in the bin folder. 
+
+##Step 3 - Testing 1,2!
+1. Turn on your speakers
+2. Open a browser and go to http://localhost:15003/speak/hello/girl
+
+##Step 4 - Import blocks in Scratch
+This only works in the offline Scratch editor (https://scratch.mit.edu/scratch2download/).
+
+1. Hold the shift-key and click the 'File' menu;
+2. Click 'Import experimental HTTP extension';
+3. Import the file 'ScratchCanTalk Extension.s2e';
+4. In the 'More blocks' section a sub-section 'ScratchCanTalk' should appear. A green light should indicate a successful connection with the helper application;
+5. the block should be self-explaining :-)
+
+
+Have fun!
--- src/ScratchCanTalk/Program.cs
+++ src/ScratchCanTalk/Program.cs
@@ -7,9 +7,7 @@ namespace ScratchCanTalk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
-
-
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(@"   _____                _       _      _____         _______    _ _    ");
             Console.WriteLine(@"  / ____|              | |     | |    / ____|       |__   __|  | | |   ");
             Console.WriteLine(@" | (___   ___ _ __ __ _| |_ ___| |__ | |     __ _ _ __ | | __ _| | | __");
@@ -25,12 +23,42 @@ namespace ScratchCanTalk
                 Port = port
             };
 
-            using (WebApp.Start<Startup>(options))
+            Console.WriteLine();
+
+            try
             {
-                Console.WriteLine("All OK!");
-                Console.WriteLine("Running a http server on port " + port);
-                Console.ReadKey();
+                using (WebApp.Start<Startup>(options))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Woohoow! The Scratch helper is running!");
+
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("The server is running on port " + port);
+
+                    Console.WriteLine();
+                    Console.WriteLine(" - Start the Scratch offline editor");
+                    Console.WriteLine(" - Click 'File' while holding the SHIFT key");
+                    Console.WriteLine(" - Load the 'ScratchCanTalk Extension.s2e'");
+                    Console.WriteLine(" - Use the 'Say'-block (in 'More blocks')");
+                    Console.WriteLine(" - Have fun ;-)");
+
+                    Console.ReadLine();
+                }
             }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Oops! Something is kaput :-(");
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Here's some more info, hope it's helpful...");
+                Console.WriteLine();
+                Console.WriteLine(e);
+                Console.ReadLine();
+
+            }
+
+           
         }
     }
 }
